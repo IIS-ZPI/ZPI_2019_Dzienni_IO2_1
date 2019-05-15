51f6d02b53ed316a5be1d71d97233264fbd9f5bd
Dominik <rakdominikrak@gmail.com>
2019-05-15 15:24:12 +0200

    dominik3131 role

--- src/lab1/InitProject.java
+++ src/lab1/InitProject.java
@@ -4,7 +4,7 @@ public class InitProject {
 
     public static void main(String[] args) {
         System.out.println("ZPI_2019_Dzienni_IO2_1 \n developer - pawo97");
-
+        System.out.println("developer - dominik3131");
     }
 
 }
