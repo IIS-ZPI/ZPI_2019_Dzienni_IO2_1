7cd3d4d6e2341b89ad67c5f1232fc584eaa9bb27
Dominik <rakdominikrak@gmail.com>
2019-05-22 14:25:48 +0200

    implemented IArithmeticsAdd

--- src/lab1/InitProject.java
+++ src/lab1/InitProject.java
@@ -1,10 +1,15 @@
 package lab1;
 
-public class InitProject {
+public class InitProject implements IArithmeticsAdd{
 
     public static void main(String[] args) {
         System.out.println("ZPI_2019_Dzienni_IO2_1 \n developer - pawo97");
 
     }
+
+	@Override
+	public double addition(double a, double b) {
+		return a+b;
+	}
 
 }
