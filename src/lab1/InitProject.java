5fadea40fd0950f046befa4d6614a6b9548f09cf
209360 <209360@L403-4.carbon.cti.p.lodz.pl>
2019-05-22 13:25:30 +0200

    InitProject implements IArithmeticsDiv

--- src/lab1/InitProject.java
+++ src/lab1/InitProject.java
@@ -1,11 +1,21 @@
 package lab1;
 
-public class InitProject {
+public class InitProject implements IArithmeticsDiv {
 
     public static void main(String[] args) {
         System.out.println("ZPI_2019_Dzienni_IO2_1 \n developer - pawo97");
         System.out.println("ZPI_2019_Dzienni_IO2_1 \n tester - Marax97");
         System.out.println("ZPI_2019_Dzienni_IO2_1 \n operations - JHinge");
         System.out.println("developer - dominik3131");
+    }
+
+    @Override
+    public double division(double A, double B) {
+        try {
+            return A / B;
+        } catch (ArithmeticException e) {
+            System.err.println("You can't divide by 0");
+        }
+        return 0;
     }
 }
