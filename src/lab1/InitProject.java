edfc898663cfcd89e9817ce51c5cc8c56c6dee4e
JHinge <zawiasa.jakub@gmail.com>
2019-05-22 15:30:40 +0200

    revert

--- src/lab1/InitProject.java
+++ src/lab1/InitProject.java
@@ -1,12 +1,8 @@
 package lab1;
-
-public class InitProject implements IArithmeticsDiff, IArithmeticsMult, IArithmeticsDiv, IArithmeticsAdd {
-
+public class InitProject implements IArithmeticsDiff,IArithmeticsMult,IArithmeticsDiv, IArithmeticsAdd{
     public static void main(String[] args) {
-    	//fantastic feature here 
         System.out.println("ZPI_2019_Dzienni_IO2_1 \n developer - pawo97");
         System.out.println("ZPI_2019_Dzienni_IO2_1 \n operations - JHinge");
-        System.out.println("tester - Marax97");
         System.out.println("developer - dominik3131");
     }
 
@@ -14,16 +10,12 @@ public class InitProject implements IArithmeticsDiff, IArithmeticsMult, IArithme
     public double Difference(double A, double B) {
         return A - B;
     }
-    
-    // comment2
-    @Override
-    public double multiplication(double a, double b) {
-    	//comment3
-        return a * b;
-    }
-
-    @Override
-    public double division(double A, double B) {
+	@Override
+	public double multiplication(double a, double b) {
+		return a * b;
+	}
+  @Override
+  public double division(double A, double B) {
         try {
             return A / B;
         } catch (ArithmeticException e) {
@@ -31,10 +23,10 @@ public class InitProject implements IArithmeticsDiff, IArithmeticsMult, IArithme
         }
         return 0;
     }
-
-    @Override
-    public double addition(double a, double b) {
-        return a + b;
-    }
+  
+	@Override
+	public double addition(double a, double b) {
+		return a+b;
+	}
 
 }
