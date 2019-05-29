be26203da05f698e5a8a94baabb8f447d64e428c
Zawiasa.Jakub <zawiasa.jakub@gmail.com>
2019-05-29 15:34:16 +0200

    project fix

---
+++ src/test/java/iis/zpi/io2_1/AppTest.java
@@ -0,0 +1,20 @@
+package iis.zpi.io2_1;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
