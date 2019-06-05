79c8680920d6f337024046021d00d7791870caf9
Dominik <rakdominikrak@gmail.com>
2019-06-05 13:24:20 +0200

    Initial version of SingleCurrency data downolading and executing rest requests

---
+++ src/main/java/com/zpi/currencyapp/DataDownloader.java
@@ -0,0 +1,98 @@
+package com.zpi.currencyapp;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.LocalDate;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import com.google.gson.Gson;
+import com.zpi.datamodel.CurrencyNoteA;
+
+public class DataDownloader {
+	//TO DO dodać obsługę błędnych zakresów dat oraz gdy pobrane dane są puste
+	public static CurrencyNoteA getDataForSingleCurrency(String currencyCode, LocalDate startDate, LocalDate EndDate) {
+		String uri=createRequestUriForSingleCurrency(currencyCode,startDate,EndDate);
+		String jsonStringData = executeRequest(uri);
+		Gson gson = new Gson();
+		CurrencyNoteA data = gson.fromJson(jsonStringData, CurrencyNoteA.class);
+		return data;
+	}
+
+	private static String createRequestUriForSingleCurrency(String currencyCode, LocalDate startDate,
+			LocalDate endDate) {
+		String slash = "/";
+		String uri = "http://api.nbp.pl/api/exchangerates/rates/a/";
+		uri+=currencyCode+slash;
+		uri+=startDate.toString()+slash;
+		uri+=endDate.toString()+slash;
+		return uri;
+	}
+
+	// "http://api.nbp.pl/api/exchangerates/rates/a/gbp/last/10/?format=json"
+	private static String executeRequest(String uri) {
+		CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+		String data = "";
+		try {
+			HttpGet httpGetRequest = new HttpGet(uri);
+
+			// Execute HTTP request
+			HttpResponse httpResponse = httpClient.execute(httpGetRequest);
+			// Get hold of the response entity
+			HttpEntity entity = httpResponse.getEntity();
+
+			// If the response does not enclose an entity, there is no need
+			// to bother about connection release
+			byte[] buffer = new byte[1024];
+			if (entity != null) {
+				InputStream inputStream = entity.getContent();
+				try {
+					int bytesRead = 0;
+					
+					BufferedInputStream bis = new BufferedInputStream(inputStream);
+					while ((bytesRead = bis.read(buffer)) != -1) {
+						data += new String(buffer, 0, bytesRead);
+					}
+					
+
+				} catch (IOException ioException) {
+					// In case of an IOException the connection will be released
+					// back to the connection manager automatically
+					ioException.printStackTrace();
+				} catch (RuntimeException runtimeException) {
+					// In case of an unexpected exception you may want to abort
+					// the HTTP request in order to shut down the underlying
+					// connection immediately.
+					httpGetRequest.abort();
+					runtimeException.printStackTrace();
+				} finally {
+					// Closing the input stream will trigger connection release
+					try {
+						inputStream.close();
+					} catch (Exception ignore) {
+					}
+				}
+			}
+		} catch (ClientProtocolException e) {
+			// thrown by httpClient.execute(httpGetRequest)
+			e.printStackTrace();
+		} catch (IOException e) {
+			// thrown by entity.getContent();
+			e.printStackTrace();
+		} finally {
+			try {
+				httpClient.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return data;
+	}
+}
---
+++ src/main/java/com/zpi/datamodel/RateA.java
@@ -0,0 +1,16 @@
+package com.zpi.datamodel;
+
+public class RateA {
+    String no;
+    String effectiveDate;
+    String mid;
+	public String getNo() {
+		return no;
+	}
+	public String getEffectiveDate() {
+		return effectiveDate;
+	}
+	public String getMid() {
+		return mid;
+	}
+}
