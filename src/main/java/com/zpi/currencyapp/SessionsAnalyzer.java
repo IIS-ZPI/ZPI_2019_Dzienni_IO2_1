19aff90df59a7aa4f8a8f30678e2f8c8f30fb62c
Marax97 <44068049+Marax97@users.noreply.github.com>
2019-06-23 02:24:47 +0200

    tested session analyzer and fixed bug in stable session calculation

--- src/main/java/com/zpi/currencyapp/SessionsAnalyzer.java
+++ src/main/java/com/zpi/currencyapp/SessionsAnalyzer.java
@@ -4,73 +4,82 @@ import com.zpi.datamodel.CurrencyNoteA;
 import com.zpi.datamodel.RateA;
 
 public class SessionsAnalyzer {
-	/**Calculates how many growth sessions are in passed amount of data
-	 * 
-	 * @param Note rates to calculate growth sessions 
-	 * @return amount of growth sessions
-	 */
-	public int calculateGrowthSessionsAmount(CurrencyNoteA Note) {
-		int growthSession=0;
-		boolean isGrowing=false;
-		RateA temp=Note.getRates().get(0);
-		for(RateA rate : Note.getRates()) {
-			if(rate.getMid()>temp.getMid()) {
-				if(!isGrowing) {
-					growthSession++;
-					isGrowing=true;
-				}
-			}
-			else {
-				isGrowing=false;
-			}
-			temp=rate;
-		}
-		return growthSession;
-	}
-	/**Calculates how many decrease sessions are in passed amount of data
-	 * 
-	 * @param Note rates to calculate decrease sessions 
-	 * @return amount of decrease sessions
-	 */
-	public int calculateDownwardSessionsAmount(CurrencyNoteA Note) {
-		int decraseSession=0;
-		boolean isDecreasing=false;
-		RateA temp=Note.getRates().get(0);
-		for(RateA rate : Note.getRates()) {
-			if(rate.getMid()<temp.getMid()) {
-				if(!isDecreasing) {
-					decraseSession++;
-					isDecreasing=true;
-				}
-			}
-			else {
-				isDecreasing=false;
-			}
-			temp=rate;
-		}
-		return decraseSession;
-	}
-	/**Calculates how many stable sessions are in passed amount of data
-	 * 
-	 * @param Note rates to calculate stable sessions 
-	 * @return amount of stable sessions
-	 */
-	public int calculatestableSessionsAmount(CurrencyNoteA Note) {
-		int stableSessions=0;
-		boolean isStable=false;
-		RateA temp=Note.getRates().get(0);
-		for(RateA rate : Note.getRates()) {
-			if(rate.getMid()==temp.getMid()) {
-				if(!isStable) {
-					stableSessions++;
-					isStable=true;
-				}
-			}
-			else {
-				isStable=false;
-			}
-			temp=rate;
-		}
-		return stableSessions;
-	}
+
+    /**
+     * Calculates how many growth sessions are in passed amount of data
+     *
+     * @param Note
+     *            rates to calculate growth sessions
+     * @return amount of growth sessions
+     */
+    public int calculateGrowthSessionsAmount(CurrencyNoteA Note) {
+        int growthSession = 0;
+        boolean isGrowing = false;
+        RateA temp = Note.getRates()
+                         .get(0);
+        for (RateA rate : Note.getRates()) {
+            if (rate.getMid() > temp.getMid()) {
+                if (!isGrowing) {
+                    growthSession++;
+                    isGrowing = true;
+                }
+            } else {
+                isGrowing = false;
+            }
+            temp = rate;
+        }
+        return growthSession;
+    }
+
+    /**
+     * Calculates how many decrease sessions are in passed amount of data
+     *
+     * @param Note
+     *            rates to calculate decrease sessions
+     * @return amount of decrease sessions
+     */
+    public int calculateDownwardSessionsAmount(CurrencyNoteA Note) {
+        int decraseSession = 0;
+        boolean isDecreasing = false;
+        RateA temp = Note.getRates()
+                         .get(0);
+        for (RateA rate : Note.getRates()) {
+            if (rate.getMid() < temp.getMid()) {
+                if (!isDecreasing) {
+                    decraseSession++;
+                    isDecreasing = true;
+                }
+            } else {
+                isDecreasing = false;
+            }
+            temp = rate;
+        }
+        return decraseSession;
+    }
+
+    /**
+     * Calculates how many stable sessions are in passed amount of data
+     *
+     * @param Note
+     *            rates to calculate stable sessions
+     * @return amount of stable sessions
+     */
+    public int calculateStableSessionsAmount(CurrencyNoteA Note) {
+        int stableSessions = 0;
+        boolean isStable = false;
+        RateA temp = Note.getRates()
+                         .get(0);
+        for (RateA rate : Note.getRates()) {
+            if (rate.getMid() == temp.getMid() && temp != rate) {
+                if (!isStable) {
+                    stableSessions++;
+                    isStable = true;
+                }
+            } else {
+                isStable = false;
+            }
+            temp = rate;
+        }
+        return stableSessions;
+    }
 }
--- src/test/java/com/zpi/currencyapp/SessionsAnalyzerTest.java
+++ src/test/java/com/zpi/currencyapp/SessionsAnalyzerTest.java
@@ -7,81 +7,111 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
 import com.zpi.datamodel.CurrencyNoteA;
 import com.zpi.datamodel.RateA;
 
 public class SessionsAnalyzerTest {
 
+    private static final String sessionsPath = "src/test/java/resources/SessionsAnalyzerData.json";
     private SessionsAnalyzer sessionsAnalyzer;
     private CurrencyNoteA noteA;
-    private RateA rateA;
-    private String sessionsPath;
-    Gson gson = new Gson();
+    private static CurrencyNoteA notaAFromFile;
 
     FileReader reader;
+
+    @BeforeClass
+    public static void setUpClass() throws FileNotFoundException {
+        JsonReader reader = new JsonReader(new FileReader(sessionsPath));
+        Gson gson = new Gson();
+        notaAFromFile = gson.fromJson(reader, CurrencyNoteA.class);
+    }
 
     @Before
     public void setUp() {
         sessionsAnalyzer = new SessionsAnalyzer();
         noteA = new CurrencyNoteA();
-        sessionsPath = "USD_2019_01_01_2019_02_02.json";
-        try {
-            reader = new FileReader(sessionsPath);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Test
-    public void calcuateGrownthSessionsForEmptyListListOfRates() {
         RateA rateA = new RateA();
         rateA.setMid(5);
         noteA.getRates()
              .add(rateA);
+    }
+
+    @Test
+    public void calcuateSessionsForOneNodeList_sessionWontChanged() {
         int growth = sessionsAnalyzer.calculateGrowthSessionsAmount(noteA);
+        int downword = sessionsAnalyzer.calculateDownwardSessionsAmount(noteA);
+        int stable = sessionsAnalyzer.calculateStableSessionsAmount(noteA);
 
         assertThat(growth, equalTo(0));
+        assertThat(downword, equalTo(0));
+        assertThat(stable, equalTo(0));
     }
 
     @Test
-    public void calculateGrownthSessionsWith_twoRates_expectOneGrowth() {
-        RateA rateA = new RateA();
-        rateA.setMid(5);
-        noteA.getRates()
-             .add(rateA);
+    public void calculateSessionsWith_twoRates_expectOneGrowth() {
         RateA rateA2 = new RateA();
         rateA2.setMid(8);
         noteA.getRates()
              .add(rateA2);
         int growth = sessionsAnalyzer.calculateGrowthSessionsAmount(noteA);
+        int downword = sessionsAnalyzer.calculateDownwardSessionsAmount(noteA);
+        int stable = sessionsAnalyzer.calculateStableSessionsAmount(noteA);
 
         assertThat(growth, equalTo(1));
+        assertThat(downword, equalTo(0));
+        assertThat(stable, equalTo(0));
     }
 
     @Test
-    public void calculateGrownthSessionsWith_twoRates_expectZeroGrowth() {
-        RateA rateA1 = new RateA();
-        rateA1.setMid(8);
+    public void calculateSessionsWith_twoRates_expectOneDownword() {
+        RateA rateA2 = new RateA();
+        rateA2.setMid(4);
         noteA.getRates()
-             .add(rateA1);
+             .add(rateA2);
+        int growth = sessionsAnalyzer.calculateGrowthSessionsAmount(noteA);
+        int downword = sessionsAnalyzer.calculateDownwardSessionsAmount(noteA);
+        int stable = sessionsAnalyzer.calculateStableSessionsAmount(noteA);
+
+        assertThat(growth, equalTo(0));
+        assertThat(downword, equalTo(1));
+        assertThat(stable, equalTo(0));
+    }
+
+    @Test
+    public void calculateSessionsWith_twoRates_expectOneStable() {
         RateA rateA2 = new RateA();
         rateA2.setMid(5);
         noteA.getRates()
              .add(rateA2);
         int growth = sessionsAnalyzer.calculateGrowthSessionsAmount(noteA);
+        int downword = sessionsAnalyzer.calculateDownwardSessionsAmount(noteA);
+        int stable = sessionsAnalyzer.calculateStableSessionsAmount(noteA);
 
         assertThat(growth, equalTo(0));
+        assertThat(downword, equalTo(0));
+        assertThat(stable, equalTo(1));
     }
 
     @Test
-    public void calculateGrownthSessionsForFirstMonthOf2019_shouldReturn() {
+    public void calculateSessionsFormFile_expectFiveGrowth() throws FileNotFoundException {
+        int growth = sessionsAnalyzer.calculateGrowthSessionsAmount(notaAFromFile);
+        assertThat(growth, equalTo(5));
+    }
 
-        CurrencyNoteA data = gson.fromJson(sessionsPath, CurrencyNoteA.class);
-        // int growth = sessionsAnalyzer.calculateGrowthSessionsAmount(reader.read);
+    @Test
+    public void calculateSessionsFormFile_expectThreeDownword() throws FileNotFoundException {
+        int growth = sessionsAnalyzer.calculateDownwardSessionsAmount(notaAFromFile);
+        assertThat(growth, equalTo(5));
+    }
 
-        // assertThat(growth, equalTo(5));
+    @Test
+    public void calculateSessionsFormFile_expectTwoStable() throws FileNotFoundException {
+        int growth = sessionsAnalyzer.calculateStableSessionsAmount(notaAFromFile);
+        assertThat(growth, equalTo(2));
     }
 }
